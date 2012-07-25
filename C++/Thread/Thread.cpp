b8c0b26cfbd5ce44839b76a32cd054dd8a0c9361
Any2Dev <any2dev@gmail.com>
2012-07-25 17:37:05 +0900

    테스트.

--- C++/Thread/Thread.cpp
+++ C++/Thread/Thread.cpp
@@ -3,9 +3,45 @@
 
 #include "stdafx.h"
 
+using namespace std;
+
+UINT gData = 0;
+UINT gMaxCnt = 100;
+UINT gSleep = 1;
+
+DWORD WINAPI ThreadFunc( LPVOID pArg )
+{
+	for ( UINT i = 0; i < gMaxCnt; i++ )
+	{
+		gData++;
+		cout << "Sub0 Tread : " << gData << endl;
+	}
+
+	return 0;
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	HANDLE hThread;
+	DWORD dwThreadID;
+
+	hThread = CreateThread( NULL, 0, ThreadFunc, NULL, NULL, &dwThreadID );
+
+	if ( NULL == hThread )
+	{
+		return -1;
+	}
+
+	for ( UINT i = 0; i < gMaxCnt; i++ )
+	{
+		gData++;
+		cout << "Main Thread : " << gData << endl;
+		Sleep( gSleep );
+	}
+
+	CloseHandle( hThread );
+
 	return 0;
 }
 
--- C++/Thread/stdafx.h
+++ C++/Thread/stdafx.h
@@ -9,7 +9,8 @@
 
 #include <stdio.h>
 #include <tchar.h>
-
+#include <Windows.h>
+#include <iostream>
 
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
